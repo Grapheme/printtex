65b5d5b6a8a762e4f57b1929f1447f3a4cf8dae0
Владимир <vkharseev@gmail.com>
2015-03-13 11:48:31 +0300

    start

--- config.php
+++ config.php
@@ -3,9 +3,12 @@ error_reporting(0);
 ini_set('display_errors', 0);
 
 define('MYSQL_HOST', 'localhost');
-define('MYSQL_USER', 'printtex_new');
-define('MYSQL_PASSWORD', 'printtex_new');
-define('MYSQL_DB', 'printtex_new');
+#define('MYSQL_USER', 'printtex_new');
+define('MYSQL_USER', 'root');
+#define('MYSQL_PASSWORD', 'printtex_new');
+define('MYSQL_PASSWORD', 'k.,jdmythtdyetnyfghfcyj');
+#define('MYSQL_DB', 'printtex_new');
+define('MYSQL_DB', 'printtex');
 
 define('PASSWORD_SALT', '73b7f');
 define('CHMOD_DIR', 0755);
